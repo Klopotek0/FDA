e0f465386f871faab8b45681fd06ca2d87d52a54
Jakub Kłopotek Główczewski <115160997+Klopotek0@users.noreply.github.com>
2023-11-20 23:26:19 +0100

    Create gta5_dataset_source.py

---
+++ data/gta5_dataset_source.py
@@ -0,0 +1,82 @@
+import os
+import os.path as osp
+from PIL import Image
+import numpy as np
+import torch
+from torch.utils import data
+import random
+
+class GTA5DataSet(data.Dataset):
+
+    def __init__(self, root, list_path, crop_size=(11, 11), resize=(11, 11), ignore_label=255, mean=(128, 128, 128), max_iters=None):
+        self.root = root  # folder for GTA5 which contains subfolder images, labels
+        self.list_path = list_path   # list of image names
+        self.crop_size = crop_size   # dst size for resize
+        self.resize = resize
+        self.ignore_label = ignore_label
+        self.mean = mean
+        self.img_ids = [i_id.strip() for i_id in open(list_path)]
+        if not max_iters==None:
+            self.img_ids = self.img_ids * int(  np.ceil(float(max_iters)/len(self.img_ids))  )
+
+        self.files = []
+
+        self.id_to_trainid = {0: 0, 1: 1}
+    def __len__(self):
+        return len(self.img_ids)
+
+    def __getitem__(self, index):
+        name = self.img_ids[index]
+        image = Image.open(osp.join(self.root, "source_images/%s" % name)).convert('RGB')
+        label = Image.open(osp.join(self.root, "source_masks/%s" % name))
+        
+        # resize
+        resize = (512, 512)
+        crop_size = (416, 320)
+
+        scale = random.choice([ 1.0, 1.25, 1.5, 1.75, 2.0])
+        new_size = (int(resize[0] * scale), int(resize[1] * scale))
+        image.resize(new_size, Image.BICUBIC)
+        label.resize(new_size, Image.NEAREST)
+
+        #RANDOM CROP
+        w, h = image.size
+        new_w, new_h = crop_size
+        left = random.randint(0, w - new_w)
+        top = random.randint(0, h - new_h)
+        image.crop((left, top, left + new_w, top + new_h)) 
+        label.crop((left, top, left + new_w, top + new_h))        
+        #random flip
+        if random.random() > 0.5:
+          image.transpose(Image.FLIP_LEFT_RIGHT)
+          label.transpose(Image.FLIP_LEFT_RIGHT)
+
+        #image = image.resize(self.resize, Image.BICUBIC)
+        #label = label.resize(self.resize, Image.NEAREST)
+
+        # (left, upper, right, lower)
+      #  left = self.resize[0]-self.crop_size[0]
+     #   upper= self.resize[1]-self.crop_size[1] THROW OUT FOR SAME RES
+
+      #  left = np.random.randint(0, high=left)
+     #   upper= np.random.randint(0, high=upper)
+     #   right= left + self.crop_size[0]
+     #   lower= upper+ self.crop_size[1]
+
+      #  image = image.crop((left, upper, right, lower))
+     #   label = label.crop((left, upper, right, lower))
+
+        image = np.asarray(image, np.float32)
+        label = np.asarray(label, np.float32)
+
+        label_copy = self.ignore_label * np.ones(label.shape, dtype=np.float32)
+        for k, v in self.id_to_trainid.items():
+            label_copy[label == k] = v
+
+        size = image.shape
+        image = image[:, :, ::-1]  # change to BGR
+        image -= self.mean
+        image = image.transpose((2, 0, 1))
+
+        return image.copy(), label_copy.copy(), np.array(size), name
+
