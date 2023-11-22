1c9bfe8140fc6e7954bd9dfda670bd60e52cf473
Jakub Kłopotek Główczewski <115160997+Klopotek0@users.noreply.github.com>
2023-11-22 23:42:41 +0100

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,8 @@
 import numpy as np
 from torch.utils import data
+from data.gta5_dataset_source import GTA5DataSet1
 from data.gta5_dataset import GTA5DataSet
+
 from data.cityscapes_dataset import cityscapesDataSet
 from data.cityscapes_dataset_label import cityscapesDataSetLabel
 from data.cityscapes_dataset_SSL import cityscapesDataSetSSL
@@ -12,7 +14,7 @@ cs_size_test = {'target': (416,320)}
 
 def CreateSrcDataLoader(args):
     if args.source == 'soruce_images':
-        source_dataset = GTA5DataSet( args.data_dir, args.data_list, crop_size=image_sizes['target'], 
+        source_dataset = GTA5DataSet1( args.data_dir, args.train_list, crop_size=image_sizes['target'], 
                                       resize=image_sizes['soruce_images'] ,mean=IMG_MEAN,
                                       max_iters=args.num_steps * args.batch_size )
     elif args.source == 'synthia':
@@ -28,6 +30,21 @@ def CreateSrcDataLoader(args):
                                          num_workers=args.num_workers, 
                                          pin_memory=True )    
     return source_dataloader
+
+def CreateValDataLoader(args):
+    if args.source == 'soruce_images':
+        val_dataset = GTA5DataSet( args.data_dir, args.val_list, crop_size=image_sizes['target'], ##
+                                      resize=image_sizes['soruce_images'] ,mean=IMG_MEAN,
+                                      max_iters=90 // 1)
+    else:
+        raise ValueError('The source dataset mush be either gta5 or synthia')
+    
+    val_dataloader = data.DataLoader( val_dataset, 
+                                         batch_size=1,
+                                         shuffle=False, 
+                                         num_workers=args.num_workers, 
+                                         pin_memory=True )    
+    return val_dataloader
 
 def CreateTrgDataLoader(args):
     if args.set == 'train' or args.set == 'trainval':
