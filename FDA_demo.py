7f0c03a9952c4528598ba270928018c63f012fb1
Jakub Kłopotek Główczewski <115160997+Klopotek0@users.noreply.github.com>
2023-11-23 15:24:47 +0100

    Update FDA_demo.py

--- FDA_demo.py
+++ FDA_demo.py
@@ -2,20 +2,28 @@ import numpy as np
 from PIL import Image
 from utils import FDA_source_to_target_np
 import scipy.misc
+import os
 
-im_src = Image.open("/content/FDA/data_robust/eval/GT/sequence_10_00002.png").convert('RGB')
-im_trg = Image.open("/content/FDA/data_robust/eval/noisyblurry/sequence_10_00024.png").convert('RGB')
 
-im_src = im_src.resize( (512,512), Image.BICUBIC )
-im_trg = im_trg.resize( (512,512), Image.BICUBIC )
+src_path = "/content/FDA/data_robust/eval/GT/sequence_10_00002.png"
+tar_path="/content/FDA/data_robust/blender_t8/seq_t80001.png"
+
+file_name = os.path.basename(tar_path)
+
+im_src = Image.open(src_path).convert('RGB')
+im_trg = Image.open(tar_path).convert('RGB')
+
+im_src = im_src.resize( (416,320), Image.BICUBIC )
+im_trg = im_trg.resize( (416,320), Image.BICUBIC )
 
 im_src = np.asarray(im_src, np.float32)
 im_trg = np.asarray(im_trg, np.float32)
 
 im_src = im_src.transpose((2, 0, 1))
 im_trg = im_trg.transpose((2, 0, 1))
+L=0.02
 
-src_in_trg = FDA_source_to_target_np( im_src, im_trg, L=0.01 )
+src_in_trg = FDA_source_to_target_np( im_src, im_trg, L=L )
 
 src_in_trg = src_in_trg.transpose((1,2,0))
 
@@ -24,5 +32,5 @@ src_in_trg = np.clip(src_in_trg, 0, 255).astype(np.uint8)
 image = Image.fromarray(src_in_trg)
 
 # Zapisz obraz
-image.save('demo_images/src_in_tar.png')
+image.save(f'demo_images/{file_name}_L{L}.png')
 
